4971a2dab1ded9622ac15d6f8bab377118af7ed0
ampedPF <ampedpf@gmail.com>
2021-09-06 13:50:26 +0200

    Cleaning up.

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
-//console.log("start of main.js");
-
 const actions = {
   current: "current",
   previous: "previous",
 };
 
-var config = "";
 var chatCommandsEnabled = false;
 let displayPrevious = true;
 let messageCurrent, messagePrevious, messageError;
@@ -49,7 +46,7 @@ let el_container, el_artists, el_album, el_track, el_previous, el_cover, el_prog
 
 function refreshInfo() {
   now = Date.now();
-  if(now > expires_at) {
+  if (now > expires_at) {
     refreshToken();
   } else {
     fetchInfo();
@@ -57,52 +54,50 @@ function refreshInfo() {
 }
 
 function fetchInfo() {
-  // set it in the wrapper
-  //console.log("fetching info...");
   spotifyApi.setAccessToken(access_token);
   spotifyApi.getMyCurrentPlayingTrack()
-  .then(function(data) {
-    if (data) {
+    .then(function (data) {
+      if (data) {
         if (data.is_playing) {
-            el_container.classList.remove("animateOut");
-            if (animateQueueEnabled) {
-              el_container.classList.add("animateQueue");
-            } else {
-              el_container.classList.add("animateIn");
-            }
-            el_container.style.opacity = 1;
-            widgetVisible = true;
-            // console.log('data', data);
-            let temp_artists = parseArtists(data.item.artists);
-            if ( track.artists != temp_artists
-              || track.album.name != data.item.album.name
-              || track.name != data.item.name) {
-              previous = { ...track };
-              track.artists = temp_artists;
-              track.album.name = data.item.album.name;
-              track.album.cover = data.item.album.images[0].url;
-              track.name = data.item.name;
-              track.url = data.item.external_urls.spotify;
-              track.duration_ms = data.item.duration_ms;
-              //console.log(track);
-              updateInfo();
-              el_container.classList.remove("animateQueue");
-            }
-            track.progress_ms = data.progress_ms;
-            updateProgress();
+          el_container.classList.remove("animateOut");
+          if (animateQueueEnabled) {
+            el_container.classList.add("animateQueue");
+          } else {
+            el_container.classList.add("animateIn");
+          }
+          el_container.style.opacity = 1;
+          widgetVisible = true;
+          let temp_artists = parseArtists(data.item.artists);
+          if (track.artists != temp_artists ||
+            track.album.name != data.item.album.name ||
+            track.name != data.item.name) {
+            previous = {
+              ...track
+            };
+            track.artists = temp_artists;
+            track.album.name = data.item.album.name;
+            track.album.cover = data.item.album.images[0].url;
+            track.name = data.item.name;
+            track.url = data.item.external_urls.spotify;
+            track.duration_ms = data.item.duration_ms;
+            updateInfo();
+            el_container.classList.remove("animateQueue");
+          }
+          track.progress_ms = data.progress_ms;
+          updateProgress();
         } else {
-            el_container.classList.remove("animateIn");
-            if (!animateQueueEnabled) {
-              el_container.classList.add("animateOut");
-            }
-            el_container.style.opacity = 0;
-            widgetVisible = false;
+          el_container.classList.remove("animateIn");
+          if (!animateQueueEnabled) {
+            el_container.classList.add("animateOut");
+          }
+          el_container.style.opacity = 0;
+          widgetVisible = false;
         }
-    }
-    setTimeout(refreshInfo(), updateRefreshRate);
-  }, function(err) {
-    console.error(err);
-  });
+      }
+      setTimeout(refreshInfo(), updateRefreshRate);
+    }, function (err) {
+      console.error(err);
+    });
 }
 
 function updateInfo() {
@@ -128,29 +123,8 @@ function animateText(elem, text, maxLength) {
 }
 
 function updateProgress() {
-  //console.log("progress_ms:", track.progress_ms);
   el_progress_text.innerText = msToTime(track.progress_ms);
   el_progress_bar.style.width = (track.progress_ms / track.duration_ms * 100) + "%";
-  //console.log("el_progress_bar.style.width", el_progress_bar.style.width);
-}
-
-// https://stackoverflow.com/questions/19700283/how-to-convert-time-in-milliseconds-to-hours-min-sec-format-in-javascript
-function msToTime(duration) {
-  var seconds = Math.floor((duration / 1000) % 60),
-    minutes = Math.floor((duration / (1000 * 60)) % 60),
-    hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
-
-  hours = (hours < 10) ? "0" + hours : hours;
-  minutes = (minutes < 10) ? "0" + minutes : minutes;
-  seconds = (seconds < 10) ? "0" + seconds : seconds;
-
-  let time = "";
-  if (hours > 0) {
-    time = hours + ":"
-  }
-  time = minutes + ":" + seconds;
-
-  return time;
 }
 
 function refreshToken() {
@@ -159,15 +133,15 @@ function refreshToken() {
   var url = 'https://accounts.spotify.com/api/token';
   var params = 'grant_type=refresh_token&refresh_token=' + refresh_token;
   http.open('POST', url, true);
-  
+
   //Send the proper header information along with the request
   http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-  
+
   var auth_string = 'Basic ' + utf8_to_b64(client_id + ':' + client_secret);
   http.setRequestHeader('Authorization', auth_string);
-  
-  http.onreadystatechange = function() {//Call a function when the state changes.
-    if(http.readyState == 4 && http.status == 200) {
+
+  http.onreadystatechange = function () { //Call a function when the state changes.
+    if (http.readyState == 4 && http.status == 200) {
       let res = JSON.parse(http.responseText);
       access_token = res.access_token;
       expires_at = Date.now() + res.expires_in * 1000;
@@ -188,36 +162,62 @@ async function sendTwitchMessage(which, track) {
       message = messagePrevious;
     }
     message = message.replaceAll("\[\[track\]\]", track.name)
-    .replaceAll('[[album]]', track.album)
-    .replaceAll('[[url]]', track.url)
-    .replaceAll('[[artists]]', track.artists);
+      .replaceAll('[[album]]', track.album)
+      .replaceAll('[[url]]', track.url)
+      .replaceAll('[[artists]]', track.artists);
   } else {
     console.log("error: ", track);
     message = messageError;
   }
-  var data = { message: message };
+  var data = {
+    message: message
+  };
   //console.log("message :", data);
 
   await fetch("https://api.streamelements.com/kappa/v2/bot/" + account_id + "/say", {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8',
-      'Authorization': "Bearer " + jwt_token
-    },
-    body: JSON.stringify(data)
-  })
-  .then(response => response.json()) 
-  .then(result => { 
-    //console.log(result)
-  })
-  .catch(err => {
-    console.log(err)
-  });
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        'Authorization': "Bearer " + jwt_token
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(result => {
+      //console.log(result)
+    })
+    .catch(err => {
+      console.log(err)
+    });
 }
 
+/* Sending message to Twitch chat via StreamElements bot */
+window.addEventListener('onEventReceived', function (obj) {
+  let data = obj.detail.event.data;
+
+  if (chatCommandsEnabled && obj.detail.listener == "message") {
+    var badge = '';
+    let message = data["text"].toLowerCase();
+    var command = message.split(" ")[0];
+
+    if (data["badges"][0]["type"]) {
+      badge = data["badges"][0]["type"];
+    }
+    if (badge === 'moderator' || badge === 'broadcaster') {
+      if (command == "{{chatCommandCurrent}}") {
+        sendTwitchMessage(actions.current, track);
+      } else if (command == "{{chatCommandPrevious}}") {
+        sendTwitchMessage(actions.previous, previous);
+      }
+    }
+  }
+});
+
+
+/* Main process */
 function main() {
-  el_container = document.getElementById("container"); 
-  el_artists = document.getElementById("artist"); 
+  el_container = document.getElementById("container");
+  el_artists = document.getElementById("artist");
   el_album = document.getElementById("album");
   el_track = document.getElementById("title");
   el_cover = document.getElementById("cover");
@@ -226,8 +226,8 @@ function main() {
   el_duration = document.getElementById("duration-total");
   el_previous = document.getElementById("previous");
 
-  
-  if(!displayPrevious) {
+
+  if (!displayPrevious) {
     el_container.style.height = "200px";
   }
 
@@ -237,7 +237,7 @@ function main() {
     console.log("An error has occurred while loading utils.js file.");
   }).then(function () {
     // Source: https://github.com/JMPerez/spotify-web-api-js
-    $.getScript("https://ampedpf.github.io/spotify-now-playing/spotify-web-api.js", function() {
+    $.getScript("https://ampedpf.github.io/spotify-now-playing/spotify-web-api.js", function () {
       console.log("spotify-web-api.js loaded successfully.");
       spotifyApi = new SpotifyWebApi();
       refreshInfo();
@@ -249,7 +249,6 @@ function main() {
 
 /* Loading from streamelements.com */
 window.addEventListener('onWidgetLoad', function (obj) {
-  //console.log("onWidgetLoad");
   const fieldData = obj.detail.fieldData;
   client_id = fieldData.client_id;
   client_secret = fieldData.client_secret;
@@ -268,48 +267,22 @@ window.addEventListener('onWidgetLoad', function (obj) {
   scrollingDelay = fieldData.scrollingDelay;
   chatCommandsEnabled = parseInt(fieldData.chatCommandsEnabled);
   animateQueueEnabled = parseInt(fieldData.animateQueueEnabled);
-  if(animateQueueEnabled) {
+  if (animateQueueEnabled) {
     let stylesheet, animateIn, animateOut, animateInDuration, animateHoldDuration, animateOutDuration;
     animateIn = fieldData.animateIn;
     animateInDuration = fieldData.animateInDuration;
     animateHoldDuration = fieldData.animateHoldDuration;
     animateOut = fieldData.animateOut;
     animateOutDuration = fieldData.animateOutDuration;
-    
+
     stylesheet = document.styleSheets[0];
-    let rule = stylesheet.cssRules[stylesheet.cssRules.length-1];
-    
-    rule.style.animation =  animateIn + " " + animateInDuration + "s, hold " + animateHoldDuration + "s " + animateInDuration + "s, " + animateOut + " " + animateOutDuration + "s " + (animateHoldDuration + animateInDuration) + "s";
+    let rule = stylesheet.cssRules[stylesheet.cssRules.length - 1];
+
+    rule.style.animation = animateIn + " " + animateInDuration + "s, hold " + animateHoldDuration + "s " + animateInDuration + "s, " + animateOut + " " + animateOutDuration + "s " + (animateHoldDuration + animateInDuration) + "s";
   }
   displayPrevious = fieldData.displayPrevious == "flex" ? true : false;
 
   main();
-});
-
-window.addEventListener('onEventReceived', function (obj) {
-  let data = obj.detail.event.data;
-
-  if (chatCommandsEnabled && obj.detail.listener == "message") {
-    var badge = '';
-    let message = data["text"].toLowerCase();
-      var command = message.split(" ")[0];
-      // var option = message.split(" ")[1];
-      
-      if (data["badges"][0]["type"]) {
-        badge = data["badges"][0]["type"];
-      }
-      //console.log(badge);
-      // console.log('{{chatCommand}}');
-      if (badge === 'moderator' || badge === 'broadcaster') {
-        if (command == "{{chatCommandCurrent}}") {
-          //console.log(command);
-          sendTwitchMessage(actions.current, track);
-        } else if (command == "{{chatCommandPrevious}}") {
-          //console.log(command);
-          sendTwitchMessage(actions.previous, previous);
-        }
-      }
-  }
 });
 
 /* */
@@ -317,7 +290,6 @@ window.addEventListener('onEventReceived', function (obj) {
 /* Loading from local env *
 $(document).ready(function() {
   $.getJSON('./config.json', function (response) {
-    // config = response;
     updateRefreshRate = response.updateRefreshRate;
     client_id = response.spotify.client_id;
     client_secret = response.spotify.client_secret;
